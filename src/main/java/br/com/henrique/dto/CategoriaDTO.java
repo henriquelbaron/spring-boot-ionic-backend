de4862309cbb565a383455157b8367c0fec1901d
Henrique <hike.lemes@gmail.com>
2019-04-03 08:28:24 -0300

    Validação email cliente update

--- src/main/java/br/com/henrique/dto/CategoriaDTO.java
+++ src/main/java/br/com/henrique/dto/CategoriaDTO.java
@@ -13,7 +13,7 @@ public class CategoriaDTO implements Serializable {
 
 	private Integer id;
 	@NotEmpty(message = "Preenchimento obrigatorio")
-	@Length(min=5,max=80,message="Tamonho deve ser entre 5 a 80 caracteres")
+	@Length(min = 5, max = 80, message = "Tamonho deve ser entre 5 a 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/br/com/henrique/dto/ClienteDTO.java
+++ src/main/java/br/com/henrique/dto/ClienteDTO.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.henrique.domain.Cliente;
-
+import br.com.henrique.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
